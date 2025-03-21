caf9685ae81511a686ec75d20efb4ddb3cc53a16
Judd Maltin <judd@newgoliath.com>
2025-03-20 20:30:57 -0400

    add newline

--- watch_vms_and_nodes.sh
+++ watch_vms_and_nodes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 watch "oc get nodes -l 'beta.kubernetes.io/instance-type=i3.metal'; \
-        echo -n 'VMs per Node:';  \
+        echo 'VMs per Node:';  \
         oc get vmi -A -o jsonpath='{.items[*].metadata.labels.kubevirt\.io/nodeName}' \
         | tr ' ' '\n'   \
         | sort  \
